8cdeb2ddbbbe21834d66a32fe852f5f7fc9c239b
Andrew Kleonskiy <co95310a@mail.ru>
2023-01-15 11:10:54 +0300

    chapter 5

---
+++ 5.1 Подключение к базе данных post.py
@@ -0,0 +1,17 @@
+import asyncio
+import asyncpg
+
+
+async def main():
+    connection = await asyncpg.connect(host='127.0.0.1',
+                                       port=5432,
+                                       user='postgres',
+                                       database='postgres',
+                                       password='password'
+                                       )
+    version = connection.get_server_version()
+    print(f'Подключено!{version}')
+    await connection.close()
+
+
+asyncio.run(main())
